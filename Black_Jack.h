a2334eefc82be744184188f20b629f6da88d5f0c
Noga <noga001@gmail.com>
2022-07-09 11:12:31 +0300

    first commit

---
+++ Black_Jack.h
@@ -0,0 +1,19 @@
+#ifndef __BLACK_JACK_H__
+#define __BLACK_JACK_H__
+/*
+ * Author: Noga Avraham
+ * Description: Header for the "Black Jack" game. [Real-Time Group C course project]
+ * Language:  C
+ * Date: July 2021
+*/
+#include <stddef.h> //size_T
+
+#define FAIL -1
+#define SUCCESS 0
+
+
+//Call this function to start playing.
+//Returns: error int number FAIL in case of fail, otherwise returns SUCCESS.
+int play();
+
+#endif //__BLACK_JACK_H__
